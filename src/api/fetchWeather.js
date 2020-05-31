b8ad2534e0b9409d7e7e96e3f200c3f418ceb5a6
Serg Nazarchuk <tesejz@gmail.com>
2020-05-31 20:58:46 +0300

    v

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const apiKey = 'f33a484cf794d08d0148764789aaba32';
 
-export const fetchWewther  = async (query)=>{
+export const get  = async (query)=>{
     const {data} = await axios.get(baseUrl, {
         params : {
             q: query,
